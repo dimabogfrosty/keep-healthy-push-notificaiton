ebc86cee291bed94a66d10d0db62181e9e6fa8ba
Dzmitry <dimabogfrosty@gmail.com>
2020-06-27 11:51:42 +0300

    Initial commit

---
+++ src/test/java/com/keep/healthy/push/notification/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keep.healthy.push.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
